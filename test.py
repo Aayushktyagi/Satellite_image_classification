7fca845da07c15261c1abd4979c323cca03dfdf3
Aayushktyagi <aayush16081@iiitd.ac.in>
2019-05-15 01:07:32 +0530

    Confusion matrix

--- test.py
+++ test.py
@@ -9,6 +9,23 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from preprocess import Preprosess
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+def get_confusion_matrix(model,test_images,test_labels):
+    '''
+    get confusion matrix
+    '''
+    test_predictions = np.round(model.predict(test_images))
+    print(test_predictions)
+    results = confusion_matrix(test_labels.argmax(axis=1),test_predictions.argmax(axis=1))
+    accuarcy = accuracy_score(test_labels.argmax(axis=1),test_predictions.argmax(axis=1))
+    classify_report = classification_report(test_labels.argmax(axis=1),test_predictions.argmax(axis=1))
+    # confusion_metrics = tf.math.confusion_matrix(test_labels,test_predictions)
+    print(results)
+    print(accuarcy)
+    print(classify_report)
 
 
 def evaluate_model(model, test_images, test_labels):
@@ -49,6 +66,9 @@ def load_model(args):
         print("Restored model, accuracy: {:5.2f}%".format(100*acc))
     else:
         predict(model, test_images , test_labels)
+        if args.show_metrics == 'True':
+            get_confusion_matrix(model,test_images,test_labels)
+
 
 
 
@@ -59,5 +79,6 @@ if __name__ == "__main__":
     parser.add_argument("--model_path",help = "Model path")
     parser.add_argument("--data_path",help = "Path to image",default = './SAT-4_and_SAT-6_datasets/sat-4-full.mat')
     parser.add_argument("--mode",choices=['predict','accuracy'],default = 'predict')
+    parser.add_argument("--show_metrics",default = 'False')
     args = parser.parse_args()
     load_model(args)
